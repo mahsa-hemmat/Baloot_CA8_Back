9e79c1d663a3d4ae7fdd3b5bde3107c5f5569350
Mahsa <92688832+mahsa-hemmat@users.noreply.github.com>
2023-05-31 17:27:46 +0330

    Update CommodityInfo.java

--- src/main/java/com/baloot/info/CommodityInfo.java
+++ src/main/java/com/baloot/info/CommodityInfo.java
@@ -3,28 +3,28 @@ package com.baloot.info;
 import com.baloot.model.Category;
 import com.baloot.model.Commodity;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.HashSet;
 import java.util.Set;
 
 public class CommodityInfo {
-    private final int id;
-    private final String name;
-    private final int providerId;
+    private int id;
+    private String name;
+    private int providerId;
     private String providerName;
-    private final int price;
-    private final Set<Category> categories;
-    private final double rating;
-    private final int inStock;
-    private final String image;
+    private int price;
+    private Set<String> categories = new HashSet<>();
+    private double rating;
+    private int inStock;
+    private String image;
     private int ratingsCount;
+    public CommodityInfo(){
 
+    }
     public CommodityInfo(Commodity commodity){
         id = commodity.getId();
         name = commodity.getName();
-        price = commodity.getPrice();
         providerId = commodity.getProviderId();
-        categories = commodity.getCategories();
+        price = commodity.getPrice();
         rating = commodity.getRating();
         inStock = commodity.getInStock();
         image = commodity.getImage();
@@ -34,47 +34,78 @@ public class CommodityInfo {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getProviderName() {
         return providerName;
+    }
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
     }
 
     public int getPrice() {
         return price;
     }
 
-    public Set<Category> getCategories() {
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Set<String> getCategories() {
         return categories;
+    }
+
+    public void setCategories(Set<String> categories) {
+        this.categories = categories;
     }
 
     public double getRating() {
         return rating;
     }
 
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     public int getInStock() {
         return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
     }
 
     public String getImage() {
         return image;
     }
 
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public int getRatingsCount() {
         return ratingsCount;
+    }
+
+    public void setRatingsCount(int ratingsCount) {
+        this.ratingsCount = ratingsCount;
     }
 
     public int getProviderId() {
         return providerId;
     }
 
-    public void setProviderName(String providerName) {
-        this.providerName = providerName;
-    }
-
-    public void setRatingsCount(int ratingsCount) {
-        this.ratingsCount = ratingsCount;
+    public void setProviderId(int providerId) {
+        this.providerId = providerId;
     }
 }
